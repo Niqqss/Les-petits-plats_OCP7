5248dc2f440b312893da69d8faf24214660f0198
Nicolas Stefanelly <nstefanelly@gmail.com>
2023-03-21 00:15:41 +0100

    interface css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,86 @@
+class RecipeCard {
+  constructor(recipe) {
+    this.recipe = recipe;
+    this.recipeCard = this.createRecipeCard();
+  }
+
+  createRecipeCard() {
+    const recipeCard = document.createElement('article');
+    recipeCard.className = 'recipe-card';
+
+    const cardPlaceholder = document.createElement('div');
+    cardPlaceholder.className = 'recipe-card-placeholder';
+    const cardTextContainer = document.createElement('div');
+    cardTextContainer.className = 'recipe-card-text-container';
+
+    const infos = document.createElement('div');
+    infos.className = 'recipe-informations';
+    const howTo = document.createElement('div');
+    howTo.className = 'recipe-how-to';
+
+    const recipeName = document.createElement('h2');
+    recipeName.textContent = this.recipe.name;
+
+    const recipeDescription = document.createElement('p');
+    recipeDescription.textContent = this.recipe.description;
+
+    const ingredientsList = document.createElement('ul');
+    this.recipe.ingredients.forEach(ingredient => {
+      const ingredientItem = document.createElement('li');
+      const ingredientName = document.createElement('span');
+      ingredientName.className = 'ingredient-name';
+      ingredientName.textContent = ingredient.ingredient;
+      ingredientItem.appendChild(ingredientName);
+      ingredientItem.innerHTML += `: ${ingredient.quantity} ${ingredient.unit || ''}`;
+      ingredientsList.appendChild(ingredientItem);
+    });
+
+    const recipeTime = document.createElement('p');
+    recipeTime.className = 'recipe-time';
+    recipeTime.textContent = ` ${this.recipe.time} min`;
+    recipeTime.insertAdjacentHTML('afterbegin', '<i class="far fa-clock"></i>');
+
+    infos.appendChild(recipeName);
+    howTo.appendChild(recipeDescription);
+    howTo.appendChild(ingredientsList);
+    infos.appendChild(recipeTime);
+
+    cardTextContainer.appendChild(infos);
+    cardTextContainer.appendChild(howTo);
+
+    recipeCard.appendChild(cardPlaceholder);
+    recipeCard.appendChild(cardTextContainer);
+
+    return recipeCard;
+  }
+
+  render(container) {
+    container.appendChild(this.recipeCard);
+  }
+}
+
 import recipes from '/assets/data/recipes.js';
 
-
-// get the DOM element where you want to display the recipes
 const recipeContainer = document.querySelector('.recipes-cards-container');
 
-// loop through the array of recipes and create HTML elements to display the information
 recipes.forEach(recipe => {
-  // create a div element to hold the recipe information
-  const recipeCard = document.createElement('article');
-  recipeCard.className = "recipe-card";
-
-  const cardPlaceholder = document.createElement('div');
-  cardPlaceholder.className = "recipe-card-placeholder";
-  const cardTextContainer = document.createElement('div');
-  cardTextContainer.className = "recipe-card-text-container";
-
-  const infos = document.createElement('div');
-  infos.className = "recipe-informations";
-  const howTo = document.createElement('div');
-  howTo.className = "recipe-how-to";
-
-  // create a heading element for the recipe name
-  const recipeName = document.createElement('h2');
-  recipeName.textContent = recipe.name;
-
-  // create a paragraph element for the recipe description
-  const recipeDescription = document.createElement('p');
-  recipeDescription.textContent = recipe.description;
-
-  // create a list element for the ingredients
-  const ingredientsList = document.createElement('ul');
-  recipe.ingredients.forEach(ingredient => {
-    const ingredientItem = document.createElement('li');
-    const ingredientName = document.createElement('span'); 
-    ingredientName.className = "ingredient-name";
-    ingredientName.textContent = ingredient.ingredient; 
-    ingredientItem.appendChild(ingredientName); 
-    ingredientItem.innerHTML += `: ${ingredient.quantity} ${ingredient.unit || ''}`; 
-    ingredientsList.appendChild(ingredientItem);
-  });
-
-  // create a paragraph element for the cooking time
-  const recipeTime = document.createElement('p');
-  recipeTime.className = "recipe-time";
-  recipeTime.textContent = ` ${recipe.time} min`;
-  recipeTime.insertAdjacentHTML('afterbegin', '<i class="far fa-clock"></i>');
-
-  // append all the elements to the recipe div
-  infos.appendChild(recipeName);
-  howTo.appendChild(recipeDescription);
-  howTo.appendChild(ingredientsList);
-  infos.appendChild(recipeTime);
-
-  cardTextContainer.appendChild(infos);
-  cardTextContainer.appendChild(howTo);
-
-  recipeCard.appendChild(cardPlaceholder);
-  recipeCard.appendChild(cardTextContainer);
-
-  // append the recipe div to the container element
-  recipeContainer.appendChild(recipeCard);
+  const recipeCard = new RecipeCard(recipe);
+  recipeCard.render(recipeContainer);
 });
+
+// SUITE DU CODE
+
+const searchInput = document.getElementById('recipe-search');
+
+searchInput.addEventListener('keypress', function (event) {
+  event.preventDefault(); // prevent form submission
+});
+
+const filterIcon = document.querySelectorAll('.filter i');
+
+filterIcon.forEach(icon => {
+  icon.addEventListener('click', () => {
+    icon.classList.toggle('fa-chevron-down');
+    icon.classList.toggle('fa-chevron-up');
+  })
+})
