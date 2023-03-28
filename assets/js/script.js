a5a33504d148b0ad030bf56cac2e8864b6402306
Nicolas Stefanelly <nstefanelly@gmail.com>
2023-03-29 01:20:49 +0200

    refacto & responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,8 +70,6 @@ recipes.forEach(recipe => {
   recipeCard.render(recipeContainer);
 });
 
-// RESTE DU CODE
-
 // RECHERCHE
 
 const searchInput = document.getElementById('recipe-search');
@@ -82,7 +80,14 @@ searchInput.addEventListener('keypress', function (event) {
 
 // FILTRES
 
-// INGREDIENTS
+function toggleFilterIcon(filterIcon, listContainer, filterInput, filterName, filter) {
+  filterIcon.classList.toggle('fa-chevron-down');
+  filterIcon.classList.toggle('fa-chevron-up');
+  listContainer.classList.toggle('visible');
+  filterInput.classList.toggle('visible');
+  filterName.classList.toggle('hidden');
+  filter.classList.toggle('expand');
+}
 
 const ingredientFilter = document.querySelector('.ingredient-filter');
 const ingredientFilterIcon = document.querySelector('.ingredient-filter i');
@@ -91,64 +96,14 @@ const ingredientFilterInput = document.querySelector('.ingredient-input');
 const ingredientFilterName = document.querySelector('.ingredient-filter p');
 
 ingredientFilterIcon.addEventListener('click', () => {
-  ingredientFilterIcon.classList.toggle('fa-chevron-down');
-  ingredientFilterIcon.classList.toggle('fa-chevron-up');
-
-  ingredientsListContainer.classList.toggle('visible');
-  ingredientFilterInput.classList.toggle('visible');
-  ingredientFilterName.classList.toggle('hidden');
-  ingredientFilter.classList.toggle('expand');
-
+  toggleFilterIcon(ingredientFilterIcon, ingredientsListContainer, ingredientFilterInput, ingredientFilterName, ingredientFilter);
   if (appliancesListContainer.classList.contains('visible')) {
-    applianceFilterIcon.classList.toggle('fa-chevron-down');
-    applianceFilterIcon.classList.toggle('fa-chevron-up');
-
-    appliancesListContainer.classList.toggle('visible');
-    applianceFilterInput.classList.toggle('visible');
-    applianceFilterName.classList.toggle('hidden');
-    applianceFilter.classList.toggle('expand');
+    toggleFilterIcon(applianceFilterIcon, appliancesListContainer, applianceFilterInput, applianceFilterName, applianceFilter);
   }
-
   if (ustensilsListContainer.classList.contains('visible')) {
-    ustensilFilterIcon.classList.toggle('fa-chevron-down');
-    ustensilFilterIcon.classList.toggle('fa-chevron-up');
-
-    ustensilsListContainer.classList.toggle('visible');
-    ustensilFilterInput.classList.toggle('visible');
-    ustensilFilterName.classList.toggle('hidden');
-    ustensilFilter.classList.toggle('expand');
+    toggleFilterIcon(ustensilFilterIcon, ustensilsListContainer, ustensilFilterInput, ustensilFilterName, ustensilFilter);
   }
-})
-
-const ingredientsList = document.createElement("ul");
-ingredientsList.className = "ingredients-list";
-ingredientsListContainer.appendChild(ingredientsList);
-
-// REFACTORISER avec flatMap()
-
-const ingredientSet = new Set();
-
-recipes.forEach(recipe => {
-  recipe.ingredients.forEach(ingredient => {
-    ingredientSet.add(ingredient.ingredient);
-  });
 });
-
-const sortedIngredients = [...ingredientSet].sort((a, b) => {
-  const aNormalized = a.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  const bNormalized = b.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  return aNormalized.localeCompare(bNormalized);
-});
-
-sortedIngredients.forEach(ingredient => {
-  let ingredientItem = document.createElement("li");
-  ingredientItem.className = "ingredient-item";
-  ingredientItem.textContent = ingredient;
-  ingredientsList.append(ingredientItem);
-});
-
-
-// APPAREILS
 
 const applianceFilter = document.querySelector('.appliance-filter');
 const applianceFilterIcon = document.querySelector('.appliance-filter i');
@@ -157,59 +112,14 @@ const applianceFilterInput = document.querySelector('.appliance-input');
 const applianceFilterName = document.querySelector('.appliance-filter p');
 
 applianceFilterIcon.addEventListener('click', () => {
-  applianceFilterIcon.classList.toggle('fa-chevron-down');
-  applianceFilterIcon.classList.toggle('fa-chevron-up');
-
-  appliancesListContainer.classList.toggle('visible');
-  applianceFilterInput.classList.toggle('visible');
-  applianceFilterName.classList.toggle('hidden');
-  applianceFilter.classList.toggle('expand');
-
+  toggleFilterIcon(applianceFilterIcon, appliancesListContainer, applianceFilterInput, applianceFilterName, applianceFilter);
   if (ingredientsListContainer.classList.contains('visible')) {
-    ingredientFilterIcon.classList.toggle('fa-chevron-down');
-    ingredientFilterIcon.classList.toggle('fa-chevron-up');
-
-    ingredientsListContainer.classList.toggle('visible');
-    ingredientFilterInput.classList.toggle('visible');
-    ingredientFilterName.classList.toggle('hidden');
-    ingredientFilter.classList.toggle('expand');
+    toggleFilterIcon(ingredientFilterIcon, ingredientsListContainer, ingredientFilterInput, ingredientFilterName, ingredientFilter);
   }
-
   if (ustensilsListContainer.classList.contains('visible')) {
-    ustensilFilterIcon.classList.toggle('fa-chevron-down');
-    ustensilFilterIcon.classList.toggle('fa-chevron-up');
-
-    ustensilsListContainer.classList.toggle('visible');
-    ustensilFilterInput.classList.toggle('visible');
-    ustensilFilterName.classList.toggle('hidden');
-    ustensilFilter.classList.toggle('expand');
+    toggleFilterIcon(ustensilFilterIcon, ustensilsListContainer, ustensilFilterInput, ustensilFilterName, ustensilFilter);
   }
-})
-
-const appliancesList = document.createElement("ul");
-appliancesList.className = "appliances-list";
-appliancesListContainer.appendChild(appliancesList);
-
-// REFACTORISER avec flatMap()
-
-const applianceSet = new Set();
-
-recipes.forEach(recipe => {
-  applianceSet.add(recipe.appliance);
 });
-
-const sortedAppliances = [...applianceSet].sort();
-
-sortedAppliances.forEach(appliance => {
-  let applianceItem = document.createElement("li");
-  applianceItem.className = "appliance-item";
-  applianceItem.textContent = appliance;
-  appliancesList.append(applianceItem);
-});
-
-
-
-// USTENSILES
 
 const ustensilFilter = document.querySelector('.ustensil-filter');
 const ustensilFilterIcon = document.querySelector('.ustensil-filter i');
@@ -218,120 +128,82 @@ const ustensilFilterInput = document.querySelector('.ustensil-input');
 const ustensilFilterName = document.querySelector('.ustensil-filter p');
 
 ustensilFilterIcon.addEventListener('click', () => {
-  ustensilFilterIcon.classList.toggle('fa-chevron-down');
-  ustensilFilterIcon.classList.toggle('fa-chevron-up');
-
-  ustensilsListContainer.classList.toggle('visible');
-  ustensilFilterInput.classList.toggle('visible');
-  ustensilFilterName.classList.toggle('hidden');
-  ustensilFilter.classList.toggle('expand');
-
+  toggleFilterIcon(ustensilFilterIcon, ustensilsListContainer, ustensilFilterInput, ustensilFilterName, ustensilFilter);
   if (ingredientsListContainer.classList.contains('visible')) {
-    ingredientFilterIcon.classList.toggle('fa-chevron-down');
-    ingredientFilterIcon.classList.toggle('fa-chevron-up');
-
-    ingredientsListContainer.classList.toggle('visible');
-    ingredientFilterInput.classList.toggle('visible');
-    ingredientFilterName.classList.toggle('hidden');
-    ingredientFilter.classList.toggle('expand');
+    toggleFilterIcon(ingredientFilterIcon, ingredientsListContainer, ingredientFilterInput, ingredientFilterName, ingredientFilter);
   }
-
   if (appliancesListContainer.classList.contains('visible')) {
-    applianceFilterIcon.classList.toggle('fa-chevron-down');
-    applianceFilterIcon.classList.toggle('fa-chevron-up');
-
-    appliancesListContainer.classList.toggle('visible');
-    applianceFilterInput.classList.toggle('visible');
-    applianceFilterName.classList.toggle('hidden');
-    applianceFilter.classList.toggle('expand');
+    toggleFilterIcon(applianceFilterIcon, appliancesListContainer, applianceFilterInput, applianceFilterName, applianceFilter);
   }
-})
+});
 
-const ustensilsList = document.createElement("ul");
-ustensilsList.className = "ustensils-list";
-ustensilsListContainer.appendChild(ustensilsList);
+function createList(listContainer, items, itemClass) {
+  const list = document.createElement("ul");
+  list.className = `${itemClass}-list`;
+  listContainer.appendChild(list);
 
-// REFACTORISER avec flatMap()
+  const itemSet = new Set();
 
-const ustensilSet = new Set();
-
-recipes.forEach(recipe => {
-  recipe.ustensils.forEach(ustensil => {
-    ustensilSet.add(ustensil);
+  items.forEach((item) => {
+    itemSet.add(item);
   });
-});
 
-const sortedUstensils = [...ustensilSet].sort((a, b) => {
-  const aNormalized = a.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  const bNormalized = b.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  return aNormalized.localeCompare(bNormalized);
-});
+  const sortedItems = [...itemSet].sort((a, b) => {
+    const aNormalized = a.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    const bNormalized = b.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    return aNormalized.localeCompare(bNormalized);
+  });
 
-sortedUstensils.forEach(ustensil => {
-  let ustensilItem = document.createElement("li");
-  ustensilItem.className = "ustensil-item";
-  ustensilItem.textContent = ustensil;
-  ustensilsList.append(ustensilItem);
-});
+  sortedItems.forEach((item) => {
+    let listItem = document.createElement("li");
+    listItem.className = `${itemClass}-item`;
+    listItem.textContent = item;
+    list.append(listItem);
+  });
+}
 
+createList(ingredientsListContainer, recipes.flatMap(recipe => recipe.ingredients.map(i => i.ingredient)), "ingredients");
+createList(appliancesListContainer, recipes.map(recipe => recipe.appliance), "appliances");
+createList(ustensilsListContainer, recipes.flatMap(recipe => recipe.ustensils), "ustensils");
 
 // SUITE
 
 const selectedFiltersContainer = document.querySelector('.selected-filters-container');
-const applianceItems = document.querySelectorAll('.appliance-item');
+const appliancesItems = document.querySelectorAll('.appliances-item');
+const ingredientsItems = document.querySelectorAll('.ingredients-item');
+const ustensilsItems = document.querySelectorAll('.ustensils-item');
 
-applianceItems.forEach(applianceListItem => {
-  applianceListItem.addEventListener('click', () => {
-    selectedFiltersContainer.classList.add("container-visible");
-    const selectedFilter = document.createElement('span');
-    selectedFilter.className = "appliance-selected-item";
-    selectedFilter.textContent = applianceListItem.textContent;
-    const selectedFilterIcon = document.createElement('i');
-    selectedFilterIcon.className = "fa-regular fa-circle-xmark";
-    selectedFilter.appendChild(selectedFilterIcon);
-    selectedFiltersContainer.appendChild(selectedFilter);
+function selectFilter(item, className) {
+  selectedFiltersContainer.classList.add('container-visible');
+  const selectedFilter = document.createElement('span');
+  selectedFilter.className = className;
+  selectedFilter.textContent = item.textContent;
+  const selectedFilterIcon = document.createElement('i');
+  selectedFilterIcon.className = 'fa-regular fa-circle-xmark';
+  selectedFilter.appendChild(selectedFilterIcon);
+  selectedFiltersContainer.appendChild(selectedFilter);
 
-    selectedFilterIcon.addEventListener('click', () => {
-      selectedFiltersContainer.removeChild(selectedFilter);
-    })
-  })
+  selectedFilterIcon.addEventListener('click', () => {
+    selectedFiltersContainer.removeChild(selectedFilter);
+  });
+}
+
+appliancesItems.forEach((appliancesItem) => {
+  appliancesItem.addEventListener('click', () => {
+    selectFilter(appliancesItem, 'appliance-selected-item');
+  });
 });
 
-const ingredientItems = document.querySelectorAll('.ingredient-item');
+ingredientsItems.forEach((ingredientsItem) => {
+  ingredientsItem.addEventListener('click', () => {
+    selectFilter(ingredientsItem, 'ingredient-selected-item');
+  });
+});
 
-ingredientItems.forEach(ingredientListItem => {
-  ingredientListItem.addEventListener('click', () => {
-    selectedFiltersContainer.classList.add("container-visible");
-    const selectedFilter = document.createElement('span');
-    selectedFilter.className = "ingredient-selected-item";
-    selectedFilter.textContent = ingredientListItem.textContent;
-    const selectedFilterIcon = document.createElement('i');
-    selectedFilterIcon.className = "fa-regular fa-circle-xmark";
-    selectedFilter.appendChild(selectedFilterIcon);
-    selectedFiltersContainer.appendChild(selectedFilter);
+ustensilsItems.forEach((ustensilsItem) => {
+  ustensilsItem.addEventListener('click', () => {
+    selectFilter(ustensilsItem, 'ustensil-selected-item');
+  });
+});
 
-    selectedFilterIcon.addEventListener('click', () => {
-      selectedFiltersContainer.removeChild(selectedFilter);
-    })
-  })
-})
-
-const ustensilItems = document.querySelectorAll('.ustensil-item');
-
-ustensilItems.forEach(ustensilListItem => {
-  ustensilListItem.addEventListener('click', () => {
-    selectedFiltersContainer.classList.add("container-visible");
-    const selectedFilter = document.createElement('span');
-    selectedFilter.className = "ustensil-selected-item";
-    selectedFilter.textContent = ustensilListItem.textContent;
-    const selectedFilterIcon = document.createElement('i');
-    selectedFilterIcon.className = "fa-regular fa-circle-xmark";
-    selectedFilter.appendChild(selectedFilterIcon);
-    selectedFiltersContainer.appendChild(selectedFilter);
-
-    selectedFilterIcon.addEventListener('click', () => {
-      selectedFiltersContainer.removeChild(selectedFilter);
-    })
-  })
-})
 
